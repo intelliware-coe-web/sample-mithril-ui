c4bc981d426c702ccf30fa36880b1f46e23cd436
mr-rampage <readonly@che.wbac.ca>
2019-02-07 00:02:56 -0500

    refactor(article) using cards

--- components/article-list.component.js
+++ components/article-list.component.js
@@ -1,9 +1,13 @@
 const ArticleList = {
-  view: (vnode) => m('dl', {class: 'uk-description-list uk-description-list-divider'},
-    vnode.attrs.articles.reduce(
-      (list, article) =>
-        list.concat(m('dt', m('a', {href: article.href, title: article.title}, article.title)))
-          .concat(m('dd', article.description)),
-      [])
+  view: (vnode) => vnode.attrs.articles.map(article =>
+    m('div', {class: 'uk-card uk-card-default uk-margin uk-width-1-1@m'}, [
+      m('div', {class: 'uk-card-header'},
+        m('div', {class: 'uk-width-expand'},
+          m('h3', {class: 'uk-card-title'},
+            m('a', {href: article.href}, article.title)))
+      ),
+      m('div', {class: 'uk-card-body uk-text-justify'},
+        m('p', article.description))
+    ])
   )
 };
