c66d3ca1ebff6666116b41596cbcfb058a347216
mr-rampage <readonly@che.wbac.ca>
2019-02-07 00:03:22 -0500

    refactor(spinner) formatting

--- components/spinner.component.js
+++ components/spinner.component.js
@@ -1,3 +1,5 @@
 const Spinner = {
-  view: () => m('div', {class: 'uk-text-center'}, m('span', {'uk-spinner': 'ratio: 4.5'}))
+  view: () =>
+    m('div', {class: 'uk-text-center'},
+      m('span', {'uk-spinner': 'ratio: 4.5'}))
 };
